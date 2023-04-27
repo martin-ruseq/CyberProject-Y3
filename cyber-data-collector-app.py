4a06c80661efa6ad78bf736d6cf0d6b89fe3f948
Marcin Rusiecki <C00263263@itcarlow.ie>
2023-04-27 15:04:17 +0100

    New features added and improved performance

--- cyber-data-collector-app.py
+++ cyber-data-collector-app.py
@@ -43,19 +43,22 @@ if selected_page == "Home":
     
 if selected_page == "Data":
     
-    # st.warning("Page under construction :construction:")
-    
-    # Define the URL to scrape
+    @st.cache_data
+    def get_data(url):
+        response = requests.get(url)
+        soup = bs4(response.content, "html.parser")
+        data_table = soup.find("table")
+        
+        # Convert the Tag object to a string
+        data_table_str = str(data_table)
+        
+        return data_table_str
+
     url = "https://www.statista.com/statistics/221390/share-of-hacking-methods-across-organizations/"
+    data_table_str = get_data(url)
 
-    # Send a request to the URL and get the response
-    response = requests.get(url)
-
-    # Parse the HTML content using BeautifulSoup
-    soup = bs4(response.content, "html.parser")
-
-    # Find the data table on the page
-    data_table = soup.find("table")
+    # Convert the string back to a Tag object if needed
+    data_table = bs4(data_table_str, "html.parser")
 
     # Check if the data table exists
     if data_table is not None:
@@ -84,84 +87,152 @@ if selected_page == "Data":
                 percentage_list.append(percentage)
 
         # Visualize the data using Streamlit
-        st.title("Global Hacking Methods in 2019")
+        st.title("Global Hacking Methods")
 
         st.markdown("""
-        This chart shows the percentage of the leading global hacking methods in 2019.
+        This chart/table shows the percentage of the leading global hacking methods.
 
-        Source: [Statista.com](https://www.statista.com/statistics/221390/share-of-hacking-methods-across-organizations/)
+        Source: [statista.com](https://www.statista.com/statistics/221390/share-of-hacking-methods-across-organizations/)
         """)
+        
+        # Create a dropdown menu for year selection
+        data_year = st.selectbox("Select Year:", ["2019", "2018", "2017", "2016", "2015"])
+        
+        # Create dropdown menu for chart type
+        view_type = st.selectbox("Select View Type:", ["Chart", "Table"])
 
         # Create a horizontal bar chart of the data
         chart_data = {"Methods": methods_list, "Percentage": percentage_list}
         chart_df = pd.DataFrame(chart_data)
 
-        fig = go.Figure(go.Bar(
-            x=chart_df['Percentage'],
-            y=chart_df['Methods'],
-            orientation='h',
-            text=chart_df['Percentage'],
-            textposition='auto',
-            hovertemplate='%{x}%<extra></extra>',
-            marker=dict(
-                color=['#FCCA3A'] * len(chart_df),
-                coloraxis=None,
-            ),
-        ))
+        if data_year == "2019" and view_type == "Chart":
+            # Create a horizontal bar chart of the data
+            hack_methods_2019 = go.Figure(go.Bar(
+                x=chart_df['Percentage'],
+                y=chart_df['Methods'],
+                orientation='h',
+                text=chart_df['Percentage'],
+                textposition='auto',
+                hovertemplate='Method: %{y}<br>Percentage: %{x}<extra></extra>',
+                marker=dict(
+                    color=['#FCCA3A'] * len(chart_df),
+                    coloraxis=None,
+                ),
+            ))
 
-        fig.update_layout(
-            xaxis=dict(showgrid = True, gridwidth = 1, gridcolor = '#E5E5E5'),
-            yaxis=dict(showgrid = False),
-            
-            xaxis_title="Percentage",
-            yaxis_title="Methods",
-            font=dict(size=16),
-            margin=dict(l=0, r=0, t=60, b=0),
-        )
-
-        st.plotly_chart(fig)
-
-        csvColumn, jsonColumn, htmlColumn = st.columns(3, gap="small")
-
-        with csvColumn:
-            
-            # Add a download button for the data as CSV
-            cvs = pd.DataFrame({'Method': methods_list, 'Percentage': percentage_list}).to_csv(index=False)
-            bs64_cvs = b64.b64encode(cvs.encode()).decode()
-            st.download_button(
-                label="Download as CSV",
-                data=bytes(cvs, encoding='utf8'),
-                file_name="hacking-methods.csv",
-                mime="text/csv",
+            hack_methods_2019.update_layout(
+                xaxis=dict(showgrid = True, gridwidth = 1, gridcolor = '#E5E5E5'),
+                yaxis=dict(showgrid = False),
+                xaxis_title="Percentage",
+                yaxis_title="Methods",
+                font=dict(size=16),
+                margin=dict(l=0, r=0, t=60, b=0),
             )
-        with jsonColumn:
             
-            # Add a download button for the data as JSON
-            json = pd.DataFrame({'Method': methods_list, 'Percentage': percentage_list}).to_json(orient='records')
-            bs64_json = b64.b64encode(json.encode()).decode()
-            st.download_button(
-                label="Download as JSON",
-                data=bytes(json, encoding='utf8'),
-                file_name="hacking-methods.json",
-                mime="text/json",
-            )
-        with htmlColumn:
+            st.plotly_chart(hack_methods_2019)
             
-            # Add a download button for the data as HTML
-            html = pd.DataFrame({'Method': methods_list, 'Percentage': percentage_list}).to_html(index=False)
-            bs64_html = b64.b64encode(html.encode()).decode()
-            st.download_button(
-                label="Download as HTML",
-                data=bytes(html, encoding='utf8'),
-                file_name="hacking-methods.html",
-                mime="text/html",
-            ) 
-    else:
-        st.write("Data table not found on page.")
-        
-    st.divider()
+            def download_buttons_4_hack_methods_chart(methods_list, percentage_list):
+                csv_column, json_column, html_column = st.columns(3, gap="small")
+                
+                # Generate CSV download button
+                with csv_column:
+                    csv = pd.DataFrame({'Method': methods_list, 'Percentage': percentage_list}).to_csv(index=False)
+                    bs64_csv = b64.b64encode(csv.encode()).decode()
+                    csv_button = st.download_button(
+                        label="Download as CSV",
+                        data=bytes(csv, encoding='utf8'),
+                        file_name="hacking-methods.csv",
+                        mime="text/csv",
+                    )
+                
+                # Generate JSON download button
+                with json_column:
+                    json = pd.DataFrame({'Method': methods_list, 'Percentage': percentage_list}).to_json(orient='records')
+                    bs64_json = b64.b64encode(json.encode()).decode()
+                    json_button = st.download_button(
+                        label="Download as JSON",
+                        data=bytes(json, encoding='utf8'),
+                        file_name="hacking-methods.json",
+                        mime="text/json",
+                    )
+                
+                # Generate HTML download button
+                with html_column:
+                    html = pd.DataFrame({'Method': methods_list, 'Percentage': percentage_list}).to_html(index=False)
+                    bs64_html = b64.b64encode(html.encode()).decode()
+                    html_button = st.download_button(
+                        label="Download as HTML",
+                        data=bytes(html, encoding='utf8'),
+                        file_name="hacking-methods.html",
+                        mime="text/html",
+                    )
+    
+                # Return the base64-encoded CSV, JSON, and HTML strings
+                return bs64_csv, bs64_json, bs64_html
+            st.markdown("*Download the data:*\n\n")
+            csv_data_, json_data, html_data = download_buttons_4_hack_methods_chart(methods_list, percentage_list)
+            
+        elif not data_year == "2019" and view_type == "Chart":
+            st.warning("No data available for the selected year :warning:")
+            
+        if data_year == "2019" and view_type == "Table":
+            st.table(chart_df)
+            st.info("To download the data, please select the chart view.")
+        elif not data_year == "2019" and view_type == "Table":
+            st.warning("No data available for the selected year :warning:")
+    
+    # Custom devider            
+    st.markdown(
+        '<hr style="border-top: 4px solid #0062AF; border-radius: 5px">',
+        unsafe_allow_html=True,)
 
+    st.title("Legal Cases under Computer Misuse Act 1990")
 
+    st.markdown("""
+    This table shows the number of legal cases under the Computer Misuse Act 1990 in different years.
+
+    Source: [web.archive.org](https://web.archive.org/web/20210321200020/https://www.computerevidence.co.uk/Cases/CMA.htm)
+    """)
+    
+    @st.cache_data
+    # Fuction to scrapes data from website and creates a table with Coputer Misuse Act cases
+    def case_table(url, selected_year):
+        response = requests.get(url)
+        soup = bs4(response.content, 'html.parser')
+        table = soup.find('table', attrs={'dir': 'LTR'})
+        if table:
+            rows = table.find_all('tr')
+            if rows:
+                cases_list = []
+                year_list = []
+                case_description_list = []
+                for row in rows[1:]:
+                    cells = row.find_all('td')
+                    if len(cells) > 1:
+                        cases = cells[0].text.strip()
+                        year = cells[1].text.strip()
+                        case_description = cells[3].text.strip()
+                        if str(selected_year) in year:
+                            cases_list.append(cases)
+                            year_list.append(year)
+                            case_description_list.append(case_description)
+                cases_df = pd.DataFrame({'Case': cases_list, 'Year': year_list, 'Description': case_description_list})
+                cases_df.index = cases_df.index + 1
+                
+                return cases_df
+
+    url = "https://web.archive.org/web/20210321200020/https://www.computerevidence.co.uk/Cases/CMA.htm"
+    cases_year = st.selectbox("Select Year:", ["2021", "2020", "2019", "2018", "2017", "2016", "2015", "2014"])
+    selected_year = int(cases_year)
+
+    cma_cases_table = case_table(url, selected_year)
+
+    st.table(cma_cases_table)
+    st.info("This data table can not be downloaded")
+    st.markdown(
+    '<hr style="border-top: 4px solid #0062AF; border-radius: 5px">',
+    unsafe_allow_html=True,)
+    
 if selected_page == "About":
     st.title("About")
     st.write("This app was created to collect cyber data from various sources and visualize it in one place. The data is collected using Python and the visualization is done using Streamlit. The app is still in development and more features will be added in the future.")
