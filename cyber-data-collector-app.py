3af31e2a401ecaddbe2f8d28efb1f649abf68c12
Marcin Rusiecki <C00263263@itcarlow.ie>
2023-05-15 16:11:27 +0100

    Hidden GitHub Repo Icon

--- cyber-data-collector-app.py
+++ cyber-data-collector-app.py
@@ -21,6 +21,7 @@ hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
+            .stActionButton {visibility: hidden;}
             </style>
             """ 
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
