beae60fcafad7c5acce58c7d2bbd8a418f160093
Marcin Rusiecki <C00263263@itcarlow.ie>
2023-05-15 16:21:13 +0100

    Streamlit Hosted Badge Hidden

--- cyber-data-collector-app.py
+++ cyber-data-collector-app.py
@@ -22,6 +22,7 @@ hide_streamlit_style = """
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             .stActionButton {visibility: hidden;}
+            ..viewerBadge_container__1QSob {visibility: hidden;}
             </style>
             """ 
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
