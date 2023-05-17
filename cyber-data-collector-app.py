c9013d6ffe75b685961d2373f57f0496ff49a7ef
Marcin Rusiecki <marcin.rusiecki@protonmail.com>
2023-05-18 00:05:32 +0100

    Check 2

--- cyber-data-collector-app.py
+++ cyber-data-collector-app.py
@@ -982,4 +982,3 @@ if selected_page == "About":
 if selected_page == "Contact":
     st.title("Contact")
     st.write("If you have any questions or suggestions, you can contact me at [LinkedIn](https://www.linkedin.com/in/marcinrusiecki/).")
-    
